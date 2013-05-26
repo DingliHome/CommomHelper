e5b8bae1625efc4b0052982efd9b0bda8688d7fa
dingli <dl@gmail.com>
2013-05-26 10:18:17 +0800

    add ImageHelper.cs

---
+++ DL.CommandHelper/ClassLibraryHelper/C#/Images/ImageHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media.Imaging;
+
+namespace ClassLibraryHelper.C_.Images
+{
+    public class ImageHelper
+    {
+        private ImageHelper _instance;
+        public ImageHelper Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    return _instance = new ImageHelper();
+                }
+                return _instance;
+            }
+        }
+
+        public void ImageToBitmapSource(string path)
+        {
+            var uri = new Uri(path);
+            var bitmapImage = new BitmapImage(uri);
+            
+        }
+    }
+}
